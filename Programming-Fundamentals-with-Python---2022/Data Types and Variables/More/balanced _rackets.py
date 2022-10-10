1b8eb9e7451ec06d072b32dc10b528a01cbb4ad4
violletta1 <villygeorgieva@yahoo.com>
2022-10-10 21:50:56 +0100

    Initial commit

--- Programming-Fundamentals-with-Python---2022/Data Types and Variables/More/balanced _rackets.py
+++ Programming-Fundamentals-with-Python---2022/Data Types and Variables/More/balanced _rackets.py
@@ -33,3 +33,35 @@ for current_string in range(num_lines):
 
 if balanced == 0:
     print("BALANCED")
+
+
+#
+#
+#
+#
+#
+#
+#
+# lines = int(input())
+#
+# is_balanced = True
+# last_bracket = ''
+# for _ in range(0, lines):
+#     current = input()
+#     if current not in ['(', ')']:
+#         continue
+#
+#     if last_bracket == '' and current == ')' or last_bracket == current:
+#         is_balanced = False
+#         break
+#
+#     last_bracket = current
+#
+# if is_balanced:
+#     print('BALANCED')
+# else:
+#     print('UNBALANCED')
+#
+#
+#
+#
