a892f1371bcc7dfedbd497e1e383f6707e91052e
violletta1 <villygeorgieva@yahoo.com>
2022-10-10 16:43:04 +0100

    Initial commit

---
+++ Programming-Fundamentals-with-Python---2022/Data Types and Variables/More/exchange_intenger.py
@@ -0,0 +1,10 @@
+first_num = int(input())
+second_num = int(input())
+
+print(f"Before:\na = {first_num}\nb = {second_num}")
+
+print(print(f"After:\na = {second_num}\nb = {first_num}"))
+
+# n Python strings, the backslash "\" is a special character, also called the "escape" character.
+# It is used in representing certain whitespace characters:
+# "\t" is a tab, "\n" is a newline, and "\r" is a carriage return.
---
+++ Programming-Fundamentals-with-Python---2022/new/README.md
@@ -0,0 +1,11 @@
+# Programming Fundamentals with Python - септември 2022
+Начало: 12 септември 2022
+12 седмици
+12 кредита
+Fundamentals
+
+https://softuni.bg/about
+
+https://softuni.bg/modules/106/fundamentals-module/1356
+
+
