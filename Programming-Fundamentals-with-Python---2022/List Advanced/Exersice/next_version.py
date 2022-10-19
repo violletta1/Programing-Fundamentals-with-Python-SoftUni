f52e2d14a923864b8f04c0a2d608678cedb14ef9
violletta1 <villygeorgieva@yahoo.com>
2022-10-19 18:18:11 +0100

    Initial commit

--- Programming-Fundamentals-with-Python---2022/List Advanced/Exersice/next_version.py
+++ Programming-Fundamentals-with-Python---2022/List Advanced/Exersice/next_version.py
@@ -1,4 +1,17 @@
-current_version = input().split(".")
+# version = [int(number) for number in input().split(".")]
+# version[-1] += 1
+# for index in range(len(version) - 1, -1, -1):
+#     if version[index] > 9:
+#         version[index] = 0
+#         if index - 1 >= 0:
+#             version[index - 1] += 1
+# print('.'.join(str(number) for number in version))
 
-for digit in current_version:
-    digit += digit
+
+current_version = [int(number) for number in input().split(".")]
+current_version[-1] += 1
+for index_num in range(len(current_version)-1, -1, -1):
+    if current_version[index_num] > 9:
+        current_version[index_num] = 0
+        current_version[index_num - 1] += 1
+print(".".join([str(number) for number in current_version]))
