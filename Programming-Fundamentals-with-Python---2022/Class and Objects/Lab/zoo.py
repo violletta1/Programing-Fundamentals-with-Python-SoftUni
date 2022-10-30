0905c676cb420d2c8e829d721911f7d05ecdbff7
violletta1 <villygeorgieva@yahoo.com>
2022-10-30 17:50:48 +0000

    Initial commit

--- Programming-Fundamentals-with-Python---2022/Class and Objects/Lab/zoo.py
+++ Programming-Fundamentals-with-Python---2022/Class and Objects/Lab/zoo.py
@@ -10,3 +10,44 @@
 # Add the animal to the zoo to the corresponding list. The species could be "mammal", "fish", or "bird".
 # On the final line, you will receive a species.
 # At the end, print the info for that species and the total count of animals in the zoo.
+class Zoo:
+    __animals = 0
+    def __init__(self, name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animals(self, species, name):
+        if species == "mammal":
+            self.mammals.append(name)
+            Zoo.__animals += 1
+        elif species == "fish":
+            self.fishes.append(name)
+            Zoo.__animals += 1
+        elif species == "bird":
+            self.birds.append(name)
+            Zoo.__animals += 1
+
+    def get_info(self, species):
+        if species == "mammal":
+            return f"Mammals in {self.name}: {', '.join(self.mammals)}\nTotal animals: {Zoo.__animals}"
+
+        elif species == "fish":
+            return f"Fishes in {self.name}: {', '.join(self.fishes)}\nTotal animals: {Zoo.__animals}"
+
+        elif species == "bird":
+            return f"Birds in {self.name}: {', '.join(self.birds)}\nTotal animals: {Zoo.__animals}"
+
+
+zoo_name = input()
+zoo = Zoo(zoo_name)
+number_of_animals = int(input())
+
+for animal in range(number_of_animals):
+    species, name = input().split()
+    zoo.add_animals(species, name)
+
+species = input()
+
+print(zoo.get_info(species))
